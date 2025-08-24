89eca9d69d8a8a9997fed9c46ea00d8a3aeb50bb
Programmer Rakibul <rakibul00206@gmail.com>
2025-08-24 12:57:16 +0600

    Login form full validation complete

--- js/login.js
+++ js/login.js
@@ -1,21 +1,54 @@
 // DOM Selectors
 const btnLogin = document.getElementById("login-btn");
-const mobileNumber = document.getElementById("mobile-number");
-const pinNumber = document.getElementById("pin-number");
 const myNum = "01888419206";
 const myPin = "1234";
 
+// Function for convert input values
+function input(id, needVal) {
+  const input = document.getElementById(id);
+  const inputVal = input.value;
+  if (!needVal) {
+    const inputNum = parseInt(inputVal);
+    return inputNum;
+  }
+
+  return inputVal;
+}
+
+// Function for input values. if input value don't have digits then return an alert
+function checkNum(val) {
+  const digit = /\d/g;
+  const isDigit = digit.test(val);
+  return isDigit;
+}
+
+// Function for Login button
 function logIn(e) {
   e.preventDefault();
 
-  let mobileVal = mobileNumber.value;
-  let pinVal = pinNumber.value;
+  const mobileVal = input("mobile-number", true);
+  const pinVal = input("pin-number", true);
 
-  if (mobileVal === "") {
+  if (mobileVal.length === 0) {
     return alert("Invalid Mobile Number");
-  }
-  if (pinVal === "") {
+  } else if (pinVal.length === 0) {
     return alert("Invalid Pin Number");
+  }
+
+  if (!checkNum(mobileVal)) {
+    return alert("Please Provide a valid number");
+  } else if (!checkNum(pinVal)) {
+    return alert("Please Provide a valid pin");
+  }
+
+  if (mobileVal.length !== 11) {
+    console.log(mobileVal.length);
+
+    return alert("Mobile number must be 11 digit");
+  } else if (pinVal.length !== 4) {
+    console.log(pinVal.length);
+
+    return alert("Pin number must be 4 digit");
   }
 
   if (myNum === mobileVal && myPin === pinVal) {
@@ -26,4 +59,5 @@ function logIn(e) {
   }
 }
 
+// Event handler for login button
 btnLogin.addEventListener("click", logIn);
